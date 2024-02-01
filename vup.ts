206d1aa5a46ca35ab398a36edbb6839358d673a9
mizchi <miz404@gmail.com>
2024-02-01 18:56:07 +0900

    refactor

--- vup.ts
+++ vup.ts
@@ -1,6 +1,6 @@
-import { ReleaseType } from "https://deno.land/std@0.214.0/semver/types.ts";
+#!/usr/bin/env -S deno run -A --ext=ts
 import { $, parseArgs } from './deps.ts';
-import { parse, increment, format } from "https://deno.land/std@0.214.0/semver/mod.ts";
+import { parse, increment, format, type ReleaseType } from "https://deno.land/std@0.214.0/semver/mod.ts";
 
 const args = parseArgs({
   args: Deno.args,
@@ -9,11 +9,12 @@ const args = parseArgs({
   }
 });
 
-const v = await $`git describe --tags --abbrev=0`.text();
-const parsed = parse(v.slice(1));
-// const rv = parse(v);
+const latest = await $`git describe --tags --abbrev=0`.text();
+const parsed = parse(latest.slice(1));
 const releaseType = args.values.type as ReleaseType ?? 'patch';
 const newTag = `v${format(increment(parsed, releaseType))}`;
 
 await $`git tag ${newTag}`;
-await $`git push origin --tags`;
+if (confirm(`Push tag ${newTag}?`)) {
+  await $`git push origin --tags`;
+}
